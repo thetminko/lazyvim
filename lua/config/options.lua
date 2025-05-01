72ce694d57fdcdf0db7f66f08f6e15691ae0ddb5
Thet Min Ko <dev.minko@gmail.com>
2025-05-01 17:47:30 -0400

    Update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.ai_cmp = false
+
 return {
   { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
