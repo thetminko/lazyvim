e7b8ca083e6c0cdca89bcdccb82305e15d2f94c4
Thet Min Ko <dev.minko@gmail.com>
2025-06-12 23:01:12 -0400

    Updated versions

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,9 @@
 vim.g.ai_cmp = false
 vim.g.lazyvim_eslint_auto_format = true
 vim.g.lazyvim_prettier_needs_config = true
+vim.opt.wrap = true
+vim.opt.spell = true
+vim.opt.spelllang = { "en" }
 
 return {
   { import = "lazyvim.plugins.extras.linting.eslint" },
